c7932e2fa568acd9c536aded26de72503bc3aeb9
LucasCamarero <l.camareroperez@ikasle.eus>
2025-11-26 21:01:13 +0100

    Finalizado

--- app/src/main/java/com/example/di_tema7_animaciones/MainActivity.kt
+++ app/src/main/java/com/example/di_tema7_animaciones/MainActivity.kt
@@ -4,20 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RawRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.di_tema7_animaciones.ui.theme.DI_Tema7_AnimacionesTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +48,27 @@ fun MainScreen() {
                 .background(MaterialTheme.colorScheme.background),
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            Text("Hola")
+            LottieLego(innerPadding)
         }
     }
+}
+
+@Composable
+fun LottieLego(innerPadding: PaddingValues) {
+    Box(modifier = Modifier.padding(innerPadding)) {
+        LottieInfinite(R.raw.logos)
+    }
+}
+
+@Composable
+fun LottieInfinite(@RawRes resId: Int) {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(resId))
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = LottieConstants.IterateForever,
+    )
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+    )
 }
